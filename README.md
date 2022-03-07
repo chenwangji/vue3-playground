497d351f22a0b5f1e72ae8d18a203a5319eadc63
chenwangji <694093637@qq.com>
2022-03-07 22:42:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-ivp5ha
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-ivp5ha)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <button @click="increment">Count is: {{ count }}</button>
+  </div>
+</template>
+
+<script>
+const { ref, onMounted } = require('vue');
+export default {
+  setup() {
+    const count = ref(0);
+    function increment() {
+      count.value++;
+    }
+
+    onMounted(() => {
+      console.log(`The initial count is ${count.value}`);
+    });
+
+    return {
+      count,
+      increment,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
