d3620ccd3a2299cc83f1345b95690d3e262282a3
chenwangji <694093637@qq.com>
2022-03-09 01:00:40 +0800

    1 file modified

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,42 @@
 <template>
   <div class="hello">
     <button @click="increment">Count is: {{ count }}</button>
+    <ul>
+      <li v-for="item in state.arr">{{item}}</li>
+    </ul>
+    <p>{{state.msg}}</p
   </div>
 </template>
 
 <script>
-const { ref, onMounted } = require('vue');
+import { ref, onMounted, reactive } from 'vue';
 export default {
   setup() {
     const count = ref(0);
+    const state = reactive({
+      arr: [1],
+    });
+
     function increment() {
       count.value++;
+      state.arr[2] = 3
+      state.msg = '3'
+
+      // console.log(state instanceof Proxy)
     }
 
     onMounted(() => {
       console.log(`The initial count is ${count.value}`);
     });
 
+    onMounted(() => {
+      console.log(`The initial count is ${count.value}131`);
+    });
+
     return {
       count,
       increment,
+      state
     };
   },
 };
